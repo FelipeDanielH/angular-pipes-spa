7cd06f16aff0629b20a6c3eb81204be4d52a5602
Felipe <felipe.daniel.henriquez@gmail.com>
2023-08-03 16:01:31 -0400

    feat(date-pipes): añade locales y muestra ejemplos en la pagina de basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -7,7 +7,11 @@ import { AppComponent } from './app.component';
 import { PrimeNgModule } from './prime-ng/prime-ng.module';
 import { SharedModule } from './shared/shared.module';
 
+// Configuracion del locale de la app
+import localeEsCl from '@angular/common/locales/es-CL';
+import {registerLocaleData } from '@angular/common';
 
+registerLocaleData(localeEsCl)
 
 @NgModule({
   declarations: [
@@ -21,7 +25,12 @@ import { SharedModule } from './shared/shared.module';
     PrimeNgModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'en-US'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
