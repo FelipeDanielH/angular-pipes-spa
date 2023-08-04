32708599ef2f261dbe80294d23c896f4c5770eeb
Felipe <felipe.daniel.henriquez@gmail.com>
2023-08-04 14:30:11 -0400

    style: ordena alfabeticamente los componentes y separa los modulos de los pipes

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,24 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ProductsRoutingModule } from './products-routing.module';
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
-import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
-import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { OrderComponent } from './pages/order/order.component';
-import { ToggleCasePipe } from './pipes/toggle-case.pipe';
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+import { ProductsRoutingModule } from './products-routing.module';
+import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
+
+// Pipes
 import { CanFlyPipe } from './pipes/can-fly.pipe';
+import { ToggleCasePipe } from './pipes/toggle-case.pipe';
 
 
 @NgModule({
   declarations: [
     BasicsPageComponent,
     NumbersPageComponent,
-    UncommonPageComponent,
     OrderComponent,
-    ToggleCasePipe,
+    UncommonPageComponent,
+
+    // Pipes
     CanFlyPipe,
+    ToggleCasePipe,
   ],
   imports: [
     CommonModule,
