aef2cb54f8e06bc826e2dafe3d82c82b295e4f1b
Felipe <felipe.daniel.henriquez@gmail.com>
2023-08-04 14:24:23 -0400

    feat: agrega pipe personalizado canfly

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -5,7 +5,8 @@ import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset'
 import { MenubarModule } from 'primeng/menubar';
 import { PanelModule } from 'primeng/panel';
-import { ToolbarModule } from 'primeng/toolbar'
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
 
 
 
@@ -16,7 +17,8 @@ import { ToolbarModule } from 'primeng/toolbar'
     FieldsetModule,
     MenubarModule,
     PanelModule,
-    ToolbarModule
+    TableModule,
+    ToolbarModule,
   ]
 })
 export class PrimeNgModule { }
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -8,6 +8,8 @@ import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.compo
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { OrderComponent } from './pages/order/order.component';
 import { ToggleCasePipe } from './pipes/toggle-case.pipe';
+import { CanFlyPipe } from './pipes/can-fly.pipe';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,7 @@ import { ToggleCasePipe } from './pipes/toggle-case.pipe';
     UncommonPageComponent,
     OrderComponent,
     ToggleCasePipe,
-
+    CanFlyPipe,
   ],
   imports: [
     CommonModule,
